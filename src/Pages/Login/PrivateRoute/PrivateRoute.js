f82a81bf645567e9076e19f01c0e658f90f2ff2b
sharifok1 <sharifok1@gmail.com>
2022-01-03 03:24:44 +0600

    update review

--- src/Pages/Login/PrivateRoute/PrivateRoute.js
+++ src/Pages/Login/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Button, CircularProgress } from "@mui/material";
 import React from "react";
 // import { Spinner } from "react-bootstrap";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
 
 const PrivetRoute = ({ children, ...rest }) => {
