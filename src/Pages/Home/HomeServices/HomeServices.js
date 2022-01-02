7513195a548bf95f0b390c71ac84d97ae9203bd7
shihab-2021 <shihab77023@gmail.com>
2022-01-02 12:28:37 +0600

    services

--- src/Pages/Home/HomeServices/HomeServices.js
+++ src/Pages/Home/HomeServices/HomeServices.js
@@ -3,7 +3,13 @@ import React from "react";
 const HomeServices = () => {
   return (
     <div>
-      <h4>This is Home Services.</h4>
+      <div className="py-5">
+        <div className="section-title mr-20 pos-rel mb-80 text-center col-xl-6 col-lg-6 offset-lg-3 offset-xl-3">
+          <h1>Services</h1>
+          <span>WHAT WE OFFER</span>
+          <h2 style={{}}>We Provides Awesome Roofling Solutions</h2>
+        </div>
+      </div>
     </div>
   );
 };
