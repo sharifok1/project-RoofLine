623d049ac85aa44401ed91fa0e8da63cd9e9a0f3
MdBayazid-star <freeelencerbayazid@gmail.com>
2022-01-02 16:27:36 +0600

    Header Ready

---
+++ src/Pages/Services/MainService/Services.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <div>
+        <h2>This is Service Page.</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
