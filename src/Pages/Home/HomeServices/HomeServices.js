b18c21d3ce792a0fd9076cbdb7f2d58c283c2da6
shihab-2021 <shihab77023@gmail.com>
2022-01-02 12:47:29 +0600

    services

--- src/Pages/Home/HomeServices/HomeServices.js
+++ src/Pages/Home/HomeServices/HomeServices.js
@@ -7,7 +7,92 @@ const HomeServices = () => {
         <div className="section-title mr-20 pos-rel mb-80 text-center col-xl-6 col-lg-6 offset-lg-3 offset-xl-3">
           <h1>Services</h1>
           <span>WHAT WE OFFER</span>
-          <h2 style={{}}>We Provides Awesome Roofling Solutions</h2>
+          <h2>We Provides Awesome Roofling Solutions</h2>
+          <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6">
+              <div class="services-wrapper mb-30">
+                <div class="services-img">
+                  <a href="https://shtheme.org/demosd/roofline/?service=single-ply-roofing">
+                    <img
+                      src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/06/01-3.jpg"
+                      alt=""
+                    />
+                  </a>
+                  <div class="services-icon">
+                    <a href="https://shtheme.org/demosd/roofline/?service=single-ply-roofing">
+                      <i class="far fa-angle-double-right"></i>
+                    </a>
+                  </div>
+                </div>
+                <div class="services-text text-center">
+                  <h4>Single Ply Roofing</h4>
+                  <p>
+                    Sed perspicis unde omnis iste natus error voluptatem
+                    accusantium doloreue laudantium totam rem aperiam{" "}
+                  </p>
+                  <div class="ser-icon">
+                    <i class="flaticon-roof-4"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-4 col-md-6">
+              <div class="services-wrapper mb-30">
+                <div class="services-img">
+                  <a href="https://shtheme.org/demosd/roofline/?service=roofing-cleaning">
+                    <img
+                      src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/06/02-1.jpg"
+                      alt=""
+                    />
+                  </a>
+                  <div class="services-icon">
+                    <a href="https://shtheme.org/demosd/roofline/?service=roofing-cleaning">
+                      <i class="fas fa-angle-double-right"></i>
+                    </a>
+                  </div>
+                </div>
+                <div class="services-text text-center">
+                  <h4>Roofing Cleaning</h4>
+                  <p>
+                    Sed perspicis unde omnis iste natus error voluptatem
+                    accusantium doloreue laudantium totam rem aperiam{" "}
+                  </p>
+                  <div class="ser-icon">
+                    <i class="flaticon-roof-2"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-4 col-md-6">
+              <div class="services-wrapper mb-30">
+                <div class="services-img">
+                  <a href="https://shtheme.org/demosd/roofline/?service=buildup-roofing">
+                    <img
+                      src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/06/03.jpg"
+                      alt=""
+                    />
+                  </a>
+                  <div class="services-icon">
+                    <a href="https://shtheme.org/demosd/roofline/?service=buildup-roofing">
+                      <i class="far fa-angle-double-right"></i>
+                    </a>
+                  </div>
+                </div>
+                <div class="services-text text-center">
+                  <h4>Buildup Roofing</h4>
+                  <p>
+                    Sed perspicis unde omnis iste natus error voluptatem
+                    accusantium doloreue laudantium totam rem aperiam{" "}
+                  </p>
+                  <div class="ser-icon">
+                    <i class="flaticon-roof-3"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
