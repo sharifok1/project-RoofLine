d0bd12e2bf2dd579b2c51b4f0e96a217b30c0c28
MdBayazid-star <freeelencerbayazid@gmail.com>
2022-01-02 12:09:28 +0600

    Working With progress

--- src/Pages/Home/HomePojects/HomeProjects.js
+++ src/Pages/Home/HomePojects/HomeProjects.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomeProjects = () => {
   return (
     <div>
-      <div className="container">
+      <div className="container my-5">
         <div className="row">
           <h4 className="text-center text_orange">Recent projects</h4>
           <h2 className="text-center">Expected A Great Experience</h2>
@@ -11,6 +11,37 @@ const HomeProjects = () => {
             <img
               className="img-fluid"
               src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/01-1.jpg"
+              alt=""
+            />
+            <img
+              className="img-fluid"
+              src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/02-2.jpg"
+              alt=""
+            />
+            <img
+              className="img-fluid vertical"
+              src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/03-1.jpg"
+              alt=""
+            />
+
+            <img
+              className="img-fluid"
+              src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/05-2.jpg"
+              alt=""
+            />
+            <img
+              className="img-fluid"
+              src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/04-1.jpg"
+              alt=""
+            />
+            <img
+              className="img-fluid horizontal"
+              src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/06-2.jpg"
+              alt=""
+            />
+            <img
+              className="img-fluid "
+              src="https://shtheme.org/demosd/roofline/wp-content/uploads/2021/07/07-1.jpg"
               alt=""
             />
           </div>
--- src/Pages/Home/HomeProgress/HomeProgress.js
+++ src/Pages/Home/HomeProgress/HomeProgress.js
@@ -2,8 +2,72 @@ import React from "react";
 
 const HomeProgress = () => {
   return (
-    <div>
-      <h4>This is a Home progress.</h4>
+    <div className="pos-rel">
+      <div className="process-bg-img"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-md-8">
+            <div className="section-title mr-20 pos-rel mb-80">
+              <h1>Process</h1>
+              <span>Working progress</span>
+              <h2>How Does We Finished Our Projects</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row align-items-end">
+          <div className="col-xl-3 col-lg-4 col-md-6 col-12">
+            <div className="progress-wrapper mb-100">
+              <div className="p-content">
+                <h2>01</h2>
+              </div>
+              <div className="progress-icon">
+                <i class="fas fa-tools"></i>
+              </div>
+              <div className="progress-text">
+                <h4>How To Started Roofing</h4>
+                <p>
+                  Sed ut perspiciatis unmnis iste natus error soluptatem
+                  accusantium dolore
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-md-6 col-12">
+            <div className="progress-wrapper mb-100">
+              <div className="p-content">
+                <h2>01</h2>
+              </div>
+              <div className="progress-icon">
+                <i class="fas fa-tools"></i>
+              </div>
+              <div className="progress-text">
+                <h4>How To Started Roofing</h4>
+                <p>
+                  Sed ut perspiciatis unmnis iste natus error soluptatem
+                  accusantium dolore
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-md-6 col-12">
+            <div className="progress-wrapper mb-100">
+              <div className="p-content">
+                <h2>01</h2>
+              </div>
+              <div className="progress-icon">
+                <i class="fas fa-tools"></i>
+              </div>
+              <div className="progress-text">
+                <h4>How To Started Roofing</h4>
+                <p>
+                  Sed ut perspiciatis unmnis iste natus error soluptatem
+                  accusantium dolore
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
